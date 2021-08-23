b54589fbaddb8bc2c1f2df3c10e3389eb5b851d4
abdullah4a <45167847+abdullah4a@users.noreply.github.com>
2021-08-23 09:58:08 +0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,6 @@
 <template>
   <v-app>
     <v-app-bar app color="green" dark>
-      <v-tool-bar-title>
-        <span class="heading-1">Vuetify - </span>
-        <span>Login </span>
-      </v-tool-bar-title>
-      <v-spacer></v-spacer>
-      <v-btn left fab small dark color="green darken-4" class="mx-2">
-        <v-icon>mdi-home</v-icon>
-      </v-btn>
-      <v-btn left fab small dark color="green darken-4" class="mx-2">
-        <v-icon></v-icon>
-      </v-btn>
-      <v-btn fab small dark color="green darken-4" class="mx-2">
-        <v-icon>mdi-account-circle</v-icon></v-btn
-      >
-      <v-btn fab small dark color="green darken-4" class="mx-2">
-        <v-icon>mdi-heart</v-icon></v-btn
-      >
     </v-app-bar>
 
     <v-main>
